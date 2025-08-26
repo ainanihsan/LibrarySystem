0b0b73edec7cc0ab1bd7ac3d19ac187c6958a32e
ainan ihsan <ainanihsan@hotmail.com>
2025-08-26 10:26:59 +0200

    Error handling in controllers

--- LibrarySystem.API/Controllers/BooksController.cs
+++ LibrarySystem.API/Controllers/BooksController.cs
@@ -8,64 +8,107 @@ namespace LibrarySystem.API.Controllers
     public class BooksController : ControllerBase
     {
         private readonly BookService.BookServiceClient _grpcClient;
-        public BooksController(BookService.BookServiceClient grpcClient)
+        private ILogger _logger;
+
+        public BooksController(BookService.BookServiceClient grpcClient , ILogger logger)
         {
             _grpcClient = grpcClient;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAllBooks()
         {
             var grpcRequest = new GetAllBooksRequest();
-            var grpcResponse = await _grpcClient.GetAllBooksAsync(grpcRequest);
-            var books = grpcResponse.Books;
-            return Ok(books);
+            try
+            {
+                var grpcResponse = await _grpcClient.GetAllBooksAsync(grpcRequest);
+                var books = grpcResponse.Books;
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while fetching all books.");
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
         }
 
         [HttpGet("most-borrowed")]
         public async Task<IActionResult> GetMostBorrowedBooks()
         {
-            var grpcRequest = new GetMostBorrowedBooksRequest();            
-            var grpcResponse = await _grpcClient.GetMostBorrowedBooksAsync(grpcRequest);
-            var books = grpcResponse.Books;
-            return Ok(books);
+            var grpcRequest = new GetMostBorrowedBooksRequest();
+            try
+            {
+                var grpcResponse = await _grpcClient.GetMostBorrowedBooksAsync(grpcRequest);
+                var books = grpcResponse.Books;
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while fetching most borrowed books.");
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
         }
 
         [HttpGet("book-stats/{bookId}")]
         public async Task<IActionResult> GetBookStats(int bookId)
         {
             var grpcRequest = new GetBookStatsRequest { Id = bookId };
-            var grpcResponse = await _grpcClient.GetBookStatsAsync(grpcRequest);
-
-            // Calculate available copies
-            int available = grpcResponse.CopiesTotal - grpcResponse.Borrowed;
-
-            var response = new
+            try
             {
-                CopiesTotal = grpcResponse.CopiesTotal,
-                BorrowedCount = grpcResponse.Borrowed,
-                AvailableCopies = available < 0 ? 0 : available
-            };
+                var grpcResponse = await _grpcClient.GetBookStatsAsync(grpcRequest);
 
-            return Ok(response);
+                // Calculate available copies
+                int available = grpcResponse.CopiesTotal - grpcResponse.Borrowed;
+
+                var response = new
+                {
+                    CopiesTotal = grpcResponse.CopiesTotal,
+                    BorrowedCount = grpcResponse.Borrowed,
+                    AvailableCopies = available < 0 ? 0 : available
+                };
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while fetching book stats.");
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
         }
 
         [HttpGet("book-same-people/{bookId}")]
         public async Task<IActionResult> GetOtherBooksBorrowedBySamePeople(int bookId)
         {
             var grpcRequest = new GetOtherBooksBorrowedBySamePeopleRequest { BookId = bookId };
-            var grpcResponse = await _grpcClient.GetOtherBooksBorrowedBySamePeopleAsync(grpcRequest);
-            var books = grpcResponse.Book;
-            return Ok(books);
+            try
+            {
+                var grpcResponse = await _grpcClient.GetOtherBooksBorrowedBySamePeopleAsync(grpcRequest);
+                var books = grpcResponse.Book;
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while fetching other books borrowed by same people.");
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
         }
 
         [HttpGet("reading-estimate/{bookId}")]
         public async Task<IActionResult> GetReadingEstimate(int bookId)
         {
             var grpcRequest = new GetReadingEstimateRequest { BookId = bookId };
-            var grpcResponse = await _grpcClient.GetReadingEstimateAsync(grpcRequest);
-            var rate = grpcResponse.AverageRate;
-            return Ok(rate);
+            try
+            {
+                var grpcResponse = await _grpcClient.GetReadingEstimateAsync(grpcRequest);
+                var rate = grpcResponse.AverageRate;
+                return Ok(rate);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while fetching reading estimate.");
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
         }
 
     }
--- LibrarySystem.API/Controllers/UsersController.cs
+++ LibrarySystem.API/Controllers/UsersController.cs
@@ -9,10 +9,12 @@ namespace LibrarySystem.API.Controllers
     public class UsersController : ControllerBase
     {
         private readonly UserService.UserServiceClient _grpcClient;
+        private ILogger _logger;
 
-        public UsersController(UserService.UserServiceClient grpcClient)
+        public UsersController(UserService.UserServiceClient grpcClient, ILogger logger)
         {
             _grpcClient = grpcClient;
+            _logger = logger;
         }
 
         [HttpGet("top-users")]
@@ -23,9 +25,17 @@ namespace LibrarySystem.API.Controllers
                 StartDate = Timestamp.FromDateTime(startDate.ToUniversalTime()),
                 EndDate = Timestamp.FromDateTime(endDate.ToUniversalTime())
             };
-            var grpcResponse = await _grpcClient.GetTopUsersAsync(grpcRequest);
-            var users = grpcResponse.User;
-            return Ok(users);
+            try
+            {
+                var grpcResponse = await _grpcClient.GetTopUsersAsync(grpcRequest);
+                var users = grpcResponse.User;
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while fetching top users.");
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
         }
         [HttpGet("user-books")]
         public async Task<IActionResult> GetUserBorrowedBooks(int id, DateTime startDate, DateTime endDate)
@@ -36,9 +46,17 @@ namespace LibrarySystem.API.Controllers
                 StartDate = Timestamp.FromDateTime(startDate.ToUniversalTime()),
                 EndDate = Timestamp.FromDateTime(endDate.ToUniversalTime())
             };
-            var grpcResponse = await _grpcClient.GetUserBorrowedBooksAsync(grpcRequest);
-            var books = grpcResponse.Book;
-            return Ok(books);
+            try
+            {
+                var grpcResponse = await _grpcClient.GetUserBorrowedBooksAsync(grpcRequest);
+                var books = grpcResponse.Book;
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while fetching user borrowed books.");
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
         }
 
     }
